88b346ff1d0969af419380964e3b167a928fc848
Art <WildandArt@users.noreply.github.com>
2022-06-20 20:42:51 +0300

    Initial Commit

---
+++ Background ImageView that scrolls with collectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Background ImageView that scrolls with collectionView
+//
+//  Created by Artemy Ozerski on 20/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
